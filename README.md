fc85d55223ad8f602c434a7afe96a235cc6e0120
prithiwis <dasprithiwis@gmail.com>
2025-02-14 17:19:49 +0530

    added all the files

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 netflix-clone
 <br>
 Author- prithiwis das
+<br>
+i am prithiwis das has made this netflix clone for my project
---
+++ index.js
@@ -0,0 +1,71 @@
+function myfunction()
+{
+    var x=document.getElementById("p1");
+    if(x.style.display=="none")
+    {
+        x.style.display="block";
+    }
+    else{
+        x.style.display="none";
+    }
+}
+
+function myfunction1()
+{
+    var y=document.getElementById("p2");
+    if(y.style.display=="none")
+    {
+        y.style.display="block";
+    }
+    else{
+        y.style.display="none";
+    }
+}
+
+function myfunction2()
+{
+    var z=document.getElementById("p3");
+    if(z.style.display=="none")
+    {
+        z.style.display="block";
+    }
+    else{
+        z.style.display="none";
+    }
+}
+
+function myfunction3()
+{
+    var a=document.getElementById("p4");
+    if(a.style.display=="none")
+    {
+        a.style.display="block";
+    }
+    else{
+        a.style.display="none";
+    }
+}
+
+function myfunction4()
+{
+    var b=document.getElementById("p5");
+    if(b.style.display=="none")
+    {
+        b.style.display="block";
+    }
+    else{
+        b.style.display="none";
+    }
+}
+
+function myfunction5()
+{
+    var c=document.getElementById("p6");
+    if(c.style.display=="none")
+    {
+        c.style.display="block";
+    }
+    else{
+        c.style.display="none";
+    }
+}
